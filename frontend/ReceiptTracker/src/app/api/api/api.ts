2dc6de81b8a879ffd0c7c1b733d4dfbb42dac874
Hegedüs Botond <botond.hegedus@barre.hu>
2025-07-13 18:12:19 +0200

    search

--- frontend/ReceiptTracker/src/app/api/api/api.ts
+++ frontend/ReceiptTracker/src/app/api/api/api.ts
@@ -1,8 +1,7 @@
 export * from './auth.service';
-import {AuthService} from './auth.service';
-import {ReceiptService} from './receipt.service';
-import {StatisticService} from './statistic.service';
-
+import { AuthService } from './auth.service';
 export * from './receipt.service';
+import { ReceiptService } from './receipt.service';
 export * from './statistic.service';
+import { StatisticService } from './statistic.service';
 export const APIS = [AuthService, ReceiptService, StatisticService];
